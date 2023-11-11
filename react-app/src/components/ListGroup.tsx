b19348851cf399fd379bfab63aa988d3fb095b34
Ananthakrishnan Balajee <mozhenboo@gmail.com>
2023-11-11 17:46:06 +0530

    Conditional Rendering, Event handling
    Conditional rendering and Handling events like mouse clicks

--- react-app/src/components/ListGroup.tsx
+++ react-app/src/components/ListGroup.tsx
@@ -1,14 +1,19 @@
-function ListGroup() {
-  const items = ["New York", "Coimbatore", "Ooty", "London", "Tokyo", "Kyoto"];
+import { MouseEvent } from "react";
 
+function ListGroup() {
+  let items = ["New York", "Coimbatore", "Ooty", "London", "Tokyo", "Kyoto"];
+  const handleClick = (event: MouseEvent) => console.log(event);
   return (
+    // fragment
     <>
       {" "}
-      // fragment
       <h1>List Group</h1>
+      {items.length === 0 && <p>No items found</p>}
       <ul className="list-group">
-        {items.map((item) => (
-          <li>{item}</li>
+        {items.map((item, index) => (
+          <li className="list-group-item" onClick={handleClick} key={item}>
+            {item}
+          </li>
         ))}
       </ul>
     </>
